4d371c86b34806c6b0f59a488109bf676636cfee
Steve Keller <stkeller@microsoft.com>
2024-01-15 09:18:02 -0700

    Adding PoC code.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("This will monitor an airport code when Tower comes online.");
+            Console.WriteLine("Enter a three- or four-character airport code, i.e. 'BOS'.  Do not use 'KBOS'");
+            Console.WriteLine("Note that US airports omit the leading 'K', but others use the full ICAO code.");
+
+            var airportCode = Console.ReadLine().ToUpper();
+
+            Console.WriteLine($"Monitoring every 30 seconds for ATC that starts with {airportCode}_ and contains 'TWR' ");
+
+            bool found = false;
+            while(!found)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string apiUrl = "https://api.vatsim.net/v2/atc/online";
+                    
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Process the successful response here
+                        string responseData = await response.Content.ReadAsStringAsync();
+                        //Console.WriteLine(responseData);
+                        
+                        if(IsTowerOnline(airportCode,responseData))
+                        {
+                            Console.WriteLine($"{airportCode} Tower is online.");
+                            Console.Beep(3000,200);
+                            Console.Beep(4000,200);
+                            Console.Beep(3000,200);
+                            return;
+
+                        }
+                        else
+                            Console.WriteLine($"No {airportCode} tower yet.");
+
+
+
+
+                    }
+                }
+                System.Threading.Thread.Sleep(30*1000);
+
+
+                
+            }
+        }
+
+        private static bool IsTowerOnline(string airportCode, string VatsimJSON)
+        {
+            JArray jsonArray = JArray.Parse(VatsimJSON);
+            Console.WriteLine(jsonArray.Count);
+
+            bool found = false;
+            
+            foreach (JObject obj in jsonArray)
+            {
+                //Console.WriteLine("Checking " + obj["callsign"]);
+
+                found = (obj["callsign"].ToString().StartsWith($"{airportCode}_") && 
+                        obj["callsign"].ToString().Contains("TWR"));
+                if (found)
+                    break;
+                    
+
+                
+            }
+            return found;
+        }
+
+    }
+}
---
+++ PowershellTesting/Get-TowerControllers-WIP.ps1
@@ -0,0 +1,35 @@
+﻿$url = "https://api.vatsim.net/v2/members/1746074/flightplans"
+$url = "https://api.vatsim.net/v2/atc/online"
+
+$result = Invoke-WebRequest -uri $url -Method Get 
+
+$result.
+
+while($true){
+    $controllers = $result.Content | ConvertFrom-Json
+
+    foreach($controller in $controllers)
+    {
+        if($controller.Callsign.Contains("TWR"))
+        {
+            
+            $datetime = [DateTime]::ParseExact($($controller.start), 'yyyy-MM-ddTHH:mm:ssZ', [System.Globalization.CultureInfo]::InvariantCulture)
+
+            # Calculate the time difference
+            $timeDifference = (Get-Date) - $datetime
+
+            # Format the result in HH:MM format
+            $formattedResult = '{0:D2}:{1:D2}' -f $timeDifference.Hours, $timeDifference.Minutes
+
+
+            $controller.Callsign + "  " + $formattedResult
+            if($controller.callsign.Contains("BOS")){
+                [console]::beep()
+                [console]::beep()
+                [console]::beep()
+            }
+        }
+
+    }
+    start-sleep -Seconds 30
+}
